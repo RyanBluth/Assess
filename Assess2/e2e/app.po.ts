f4b2fcc0eb0187b333d93e79675557c4d4aca675
Ryan Bluth <ryanbluth93@gmail.com>
2016-08-06 17:32:48 -0400

    Revert

--- Assess2/e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class AssessPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('assess-app h1')).getText();
-  }
-}
--- Assess2/src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export {environment} from './environment';
-export {AppComponent} from './component/app.component';
